21cfedf19121e9fddb123270403dfae4910c7666
Amadey Vorontsov <vorontsov.aa@phystech.edu>
2023-02-12 15:10:30 +0300

    save

--- tests/src/gtest.cpp
+++ tests/src/gtest.cpp
@@ -1,19 +1,30 @@
 #include <gtest/gtest.h>
+#include <fstream>
+#include "intersect.hpp"
 
-using namespace std;
+#ifndef PROJECT_DIR_PATH
+  #define PROJECT_DIR_PATH ""
+#endif
 
-TEST(TestGroupName, Subtest_1) {
-  ASSERT_TRUE(1 == 1);
+std::string concat(std::string str) {
+    return PROJECT_DIR_PATH + str;
 }
 
-TEST(TestGroupName, Subtest_2) {
-  ASSERT_FALSE('b' == 'b');
-  cout << "continue test after failure" << endl;
+TEST(TriangleXTriangle, TestNumber0) {   
+    std::ifstream file(concat("/test/trXtr/test1.txt"));
+    
+    GeomObj::Triangle first;
+    GeomObj::Triangle second;
+    bool result;
+
+    file >> first >> second >> result;
+    
+    EXPECT_EQ(GeomObj::IntersectTriangles(first, second), result);
+    
+    file.close();
 }
 
-int main(int argc, char **argv)
-{
-  ::testing::InitGoogleTest(&argc, argv);
-
-  return RUN_ALL_TESTS();
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
--- tests/src/test_creater.cpp
+++ tests/src/test_creater.cpp
@@ -9,29 +9,28 @@ int main(int argc, char const *argv[])
     std::random_device rd;
     std::mt19937 gen(rd());
 
-    //std::uniform_int_distribution<> zone(0, 8);женишь
-    std::uniform_int_distribution<> rand(-1000, 1000);
-    std::uniform_int_distribution<> randlol(-50, 50);
+    std::uniform_int_distribution<> bigRand(-1000, 1000);
+    std::uniform_int_distribution<> smallRand(-50, 50);
 
     out << "10000 \n";
 
     for (int i = 0; i < 10000; i++)
     {
-        auto coord_1 = rand(gen);
-        auto coord_2 = rand(gen);
-        auto coord_3 = rand(gen);
+        auto coord_1 = bigRand(gen);
+        auto coord_2 = bigRand(gen);
+        auto coord_3 = bigRand(gen);
 
         out << coord_1 << " "
             << coord_2 << " "
             << coord_3 << "\t";
 
-        out << coord_1 + randlol(gen) << " "
-            << coord_2 + randlol(gen) << " "
-            << coord_3 + randlol(gen) << "\t";
+        out << coord_1 + smallRand(gen) << " "
+            << coord_2 + smallRand(gen) << " "
+            << coord_3 + smallRand(gen) << "\t";
 
-        out << coord_1 + randlol(gen) << " "
-            << coord_2 + randlol(gen) << " "
-            << coord_3 + randlol(gen) << "\n";
+        out << coord_1 + smallRand(gen) << " "
+            << coord_2 + smallRand(gen) << " "
+            << coord_3 + smallRand(gen) << "\n";
     }
 
     return 0;
