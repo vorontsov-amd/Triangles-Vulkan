1358bb600e365abb166dfc701749702f8f4c03e7
Amadey Vorontsov <vorontsov.aa@phystech.edu>
2023-03-21 21:27:12 +0300

    delete files

--- vulkan/include/sourcePath.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#define VERT_SHADER_PATH "/home/amadey/Документы/Projects/Triangles/vulkan/shaders/vert.spv"
-#define FRAG_SHADER_PATH "/home/amadey/Документы/Projects/Triangles/vulkan/shaders/frag.spv"
